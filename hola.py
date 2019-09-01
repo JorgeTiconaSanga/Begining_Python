ee8c1de22b446da24aa6f09007faa5622cc59a11
Jorge Hernan Ticona Sanga <hernan_davion7@hotmail.com>
2019-09-01 19:19:55 -0400

    pizza

--- hola.py
+++ hola.py
@@ -8,5 +8,6 @@ Created on Sun Sep  1 10:17:44 2019
 hungry = input ("are you hungry?")
 if hungry == "yes":
     print("eat samosa")
+    print("eat pizza")
 else:
     print("do your homework")    
