b0c37f16d0fe07dac2c9c62315d026fd6fd760e5
Jorge Hernan Ticona Sanga <hernan_davion7@hotmail.com>
2019-09-01 18:19:03 -0400

    second change

--- hola.py
+++ hola.py
@@ -5,5 +5,8 @@ Created on Sun Sep  1 10:17:44 2019
 @author: HERNA
 """
 
-print ("hola")
-
+hungry = input ("are you hungry?")
+if hungry == "yes":
+    print("eat samosa")
+else:
+    print("do your homework")    
